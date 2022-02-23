dc00fc6d69d68dab390a6504292ff3d364df9a84
Mouez98 <mouez.matoui@gmail.com>
2022-02-23 17:52:00 +0100

    using the token for requests to protected resources

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,29 +1,36 @@
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import AuthContext from '../../store/auth-contex';
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import AuthContext from "../../store/auth-contex";
 
-import classes from './MainNavigation.module.css';
+import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
- const authCtx = useContext(AuthContext);
+  const authCtx = useContext(AuthContext);
 
- const isLoggedIn = authCtx.isLoggedIn;
+  const isLoggedIn = authCtx.isLoggedIn;
+
   return (
     <header className={classes.header}>
-      <Link to='/'>
+      <Link to="/">
         <div className={classes.logo}>React Auth</div>
       </Link>
       <nav>
         <ul>
-          {!isLoggedIn && <li>
-            <Link to='/auth'>Login</Link>
-          </li>}
-         {isLoggedIn && <li>
-            <Link to='/profile'>Profile</Link>
-          </li>}
-          {isLoggedIn && <li>
-            <button>Logout</button>
-          </li>}
+          {!isLoggedIn && (
+            <li>
+              <Link to="/auth">Login</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <button>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,46 @@
-import classes from './ProfileForm.module.css';
+import { useRef, useContext } from "react";
+import AuthContext from "../../store/auth-contex";
+
+import classes from "./ProfileForm.module.css";
 
 const ProfileForm = () => {
+  const enteredPasswordRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredPassword = enteredPasswordRef.current.value;
+
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyB4nuNJFr4S6ceGOYWuZdnavN25466Bcao",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredPassword,
+          returnSecureToken: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(res => {
+      //Assuming: always succeeds
+    })
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" ref={enteredPasswordRef} minLength='7' />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
